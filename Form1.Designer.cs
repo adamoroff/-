b5af21d69bd5d5844954876afc9b020176859255
adamoroff <36341243+adamoroff@users.noreply.github.com>
2019-05-05 15:12:57 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Грузчик
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(-1, 50);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(800, 190);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Имя";
+            this.columnHeader1.Width = 320;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Размер";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Прямая ссылка";
+            this.columnHeader3.Width = 350;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Очередь";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(7, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Сейчас: ";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(11, 275);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(775, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::Грузчик.Properties.Resources.add;
+            this.pictureBox1.Location = new System.Drawing.Point(726, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(104, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(616, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::Грузчик.Properties.Resources.download;
+            this.pictureBox2.Location = new System.Drawing.Point(760, 7);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(28, 28);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(381, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 999;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 314);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Загрузчик";
+            this.TransparencyKey = System.Drawing.Color.Maroon;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Грузчик
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Click += (s, e) => { AddQueue(textBox1.Text); };
+        }
+
+        public void AddQueue(string url)
+        {
+            using (WebClient wb = new WebClient())
+            {
+                /*Почему-то у меня при работе с вебклиентом постоянно проблемы с большинством сайтов, 
+                  выскакивает ошибка при подключении по защищенному каналу SSL, TLS и пр. Столбец ниже решает эту проблему. Хотя пишут что це небезопасно. 
+                  Странно что у других и без этой херни работает.*/
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+                try
+                {
+                    wb.OpenRead(url);
+                }
+                catch
+                {
+                    MessageBox.Show("Это не ссылка");
+                    return;
+                }
+                string name = Path.GetFileName(url);
+                string size = "";
+                double dsize = (Convert.ToDouble(wb.ResponseHeaders["Content-Length"]) / 1048576);
+                if (dsize > 1)  // если больше одного мб
+                    size = dsize.ToString("#.# МБ"); //Покажи в мбайтах.
+                else
+                    size = (dsize * 1024).ToString("#.# КБ"); //если меньше 1мб то покажи в кбайтах.
+
+                string[] str = new string[] { size, url }; //Массив для подобъектов в listView.
+                listView1.Items.Add(name).SubItems.AddRange(str);
+                textBox1.Text = "";
+            }
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            if(listView1.Items.Count <= 0)
+            {
+                MessageBox.Show("Загрузка завершена");
+                return; //Если больше нечего загружать то не выполнять дальше.
+            }
+            using(WebClient wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += (f, r) => 
+                {
+                    progressBar1.Value = r.ProgressPercentage;  label2.Text = $"Сейчас идёт загрузка: {listView1.Items[0].Text}. Размер : {listView1.Items[0].SubItems[1].Text}";
+                    speed = r.BytesReceived;
+                    label3.Text = $"Скорость: {((double)sum2 / 1024).ToString("#.# КБ/сек")}";
+                };
+                wc.DownloadFileCompleted += (f, r) =>
+                {
+                    try
+                    {
+                        listView1.Items.RemoveAt(0);
+                        PictureBox2_Click(null, null);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Загрузки завершены");
+                    }
+                    label2.Text = "Сейчас ничего не происходит.";
+                    label3.Text = "";
+                };
+                wc.DownloadFileAsync(new Uri(listView1.Items[0].SubItems[2].Text), listView1.Items[0].Text);
+                timer1.Start(); //Запуск таймера
+            }
+        }
+
+        long speed = 0;
+        long sum = 0;
+        long sum2 = 0;
+        private void Timer1_Tick(object sender, EventArgs e) //таймер чтобы расчитать скорость в секунду.
+        {
+            sum2 = speed - sum;
+            sum = speed;
+        }
+    }
+}
